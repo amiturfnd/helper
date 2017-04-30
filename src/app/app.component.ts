00376268af71456a625b79f3a75c80f5435fd755
amiturfnd <amiturfnd@gmail.com>
2017-04-30 11:47:49 +0530

    made default scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,36 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html'
 })
+
 export class AppComponent {
-  title = 'app works!';
+	username = "Guest";
+	toDoList = [{task:"Meet Gunapathi", completed: false}];
+  newTask = "";
+
+  addTask = function () {
+  	if (this.newTask != "") { 
+  		this.toDoList.push({task:this.newTask, completed:false});
+  		this.newTask = "";
+  	}
+  }
+
+  removeTask = function (index) {
+  	this.toDoList.splice(index,1);
+  }
+
+  completeTask = function (index) {
+  	this.toDoList[index].completed = !this.toDoList[index].completed;
+  }
+
+  completeAllTask = function () {
+  	for(let item of this.toDoList) {
+		  item.completed = true;
+		}
+  }
+
+  removeAllTask = function () {
+  	this.toDoList = []
+  }
 }
