e8fa75347b0e4d8105fa1686289b70fe9d96661a
amiturfnd <amiturfnd@gmail.com>
2017-05-01 02:16:49 +0530

    improvements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,32 +6,42 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-	username = "Guest";
-	toDoList = [{task:"Meet Gunapathi", completed: false}];
-  newTask = "";
+	username = localStorage.username || "Guest"
+	changeGuest = function () {
+		localStorage.setItem("username", this.username);
+	}
 
+	toDoList = localStorage.toDoList ? JSON.parse(localStorage.toDoList) : [{task:"Set your goan for today.", completed:false}]
+  newTask = "";
+  updateToDoLists = function () {
+  	localStorage.toDoList = JSON.stringify(this.toDoList);
+  }
   addTask = function () {
   	if (this.newTask != "") { 
-  		this.toDoList.push({task:this.newTask, completed:false});
+  		this.toDoList.unshift({task:this.newTask, completed:false});
   		this.newTask = "";
+  		this.updateToDoLists()
   	}
   }
-
   removeTask = function (index) {
   	this.toDoList.splice(index,1);
+  	this.updateToDoLists()
   }
-
   completeTask = function (index) {
   	this.toDoList[index].completed = !this.toDoList[index].completed;
+  	this.updateToDoLists()
   }
-
   completeAllTask = function () {
+  	if(confirm("Are you sure, you want to complete all tasks?") != true) return;
   	for(let item of this.toDoList) {
 		  item.completed = true;
 		}
+		this.updateToDoLists()
   }
 
   removeAllTask = function () {
+  	if(confirm("Are you sure, you want to delete all tasks?") != true) return;
   	this.toDoList = []
+  	this.updateToDoLists()
   }
 }
